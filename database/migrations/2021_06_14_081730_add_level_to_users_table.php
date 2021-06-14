6e8ebf6195a61d073e8ff0b09286322b8868e4d5
Merta Yasa <made.b4li@gmail.com>
2021-06-14 18:09:51 +0800

    Initial commit kepgeawaian

---
+++ database/migrations/2021_06_14_081730_add_level_to_users_table.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddLevelToUsersTable extends Migration
+{
+    public function up(){
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('level')->default(0);
+        });
+    }
+
+    public function down(){
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('level');
+        });
+    }
+}
