111420df53a30a15f36107f90798a58a1f8c2c77
adigunawan <adigunawan0499@gmail.com>
2021-07-03 00:14:43 +0800

    Update Profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,37 +30,6 @@ class UserController extends Controller
         return view('profile_user.edit');
     }
 
-
-
-    public function updateProfile(User $user, Request $request){
-        if(Auth::id() != $user->id){
-            abort(403);
-        }
-
-        try{
-            $data = $request->all();
-            if($data['password']){
-                $data['password'] = bcrypt($data['password']);
-            }else{
-                unset($data['password']);
-            }
-
-            $this->userRepo->update($user, $data);
-
-            if(checkUserLevel() == 1){
-                $this->employeeRepo->update($user->employee, $data);
-            }
-            if(checkUserLevel() == 2){
-                $this->customerRepo->update($user->customer, $data);
-            }
-        }catch(Exception $e){
-            Log::info($e->getMessage());
-            return redirect()->back()->withInput()->with('error', 'Gagal mengubah profil '. $user->name);
-        }
-
-        return redirect()->back()->with('success', 'Berhasil mengubah profil '. $user->name);
-    }
-
     public function update(Request $request, $id){
         //
     }
